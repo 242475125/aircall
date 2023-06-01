b48f5cd01dc4b66795058fe7b61dd6dd5dd60aa6
242475125 <tyexe123@gmail.com>
2023-05-31 19:41:42 -0700

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,16 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       }
     ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HtmlWebPackPlugin({
